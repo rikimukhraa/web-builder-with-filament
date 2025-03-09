fd5b64426215c8320acdfd144ff314ce98e9bdd9
rikimukhraa <rikimukhraa@protonmail.com>
2025-03-10 02:51:59 +0700

    firt

--- resources/views/livewire/skeleton/ske-section.blade.php
+++ resources/views/livewire/skeleton/ske-section.blade.php
@@ -1,5 +1,1 @@
-<div class="w-full max-w-screen ">
-    <div role="status" class=" animate-pulse">
-        <div class="h-20 bg-gray-200 rounded-xl dark:bg-zinc-900 w-24 mb-8 mx-auto"></div>
-    </div>
-</div>
+<div class="h-20 bg-gray-200 rounded-xl dark:bg-zinc-900 w-24 mb-8 mx-auto"></div>
